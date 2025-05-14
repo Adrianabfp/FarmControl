7f36158f605db2b6690171acba9880cc56cb1a8a
Adrianabfp <dricabap@hotmail.com>
2025-05-14 08:24:23 -0300

    Atualiza o arquivo views.py em usuarios

--- usuarios/views.py
+++ usuarios/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from rolepermissions.decorators import has_permission_decorator
 
+
+@has_permission_decorator ('cadastrar_atendente')
 def cadastrar_atendente(request):
     return HttpResponse ('teste')
 
